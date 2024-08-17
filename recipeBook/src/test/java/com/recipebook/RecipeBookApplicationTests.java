bfd9b9b6da7f703d6bbf99b2b7346dd5cefa82fe
Theo Kretz <e52004057@student.tuwien.ac.at>
2024-08-17 15:35:12 +0200

    first commit again

---
+++ recipeBook/src/test/java/com/recipebook/RecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recipebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
